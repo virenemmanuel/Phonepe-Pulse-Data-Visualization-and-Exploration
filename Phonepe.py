1bfecd61a5adf1cd3570d291719603717ba6b0a0
virenemmanuel <viren.emmanuel@gmail.com>
2024-03-19 07:10:22 +0530

    adding streamlit code for visualization

--- Phonepe.py
+++ Phonepe.py
@@ -1,191 +1,167 @@
 # [File handling library]
-import os
-import json
+import streamlit as st
+from streamlit_option_menu import option_menu 
+import psycopg2
+import plotly.express as px
+# import os
+# import json
 import pandas as pd
 
 
+#Dataframe creation
 
-#Aggre_transaction
+#sql connection
+mydb = psycopg2.connect (host = "localhost",
+                         user = "postgres",
+                         password = "roomno13",
+                         database = "phonepe_data",
+                         port = "5432")
+cursor = mydb.cursor()
 
-path1 = "C:/Users/viren/OneDrive/Desktop/IIT-MADARAS(GUVI)/Phonepay project/pulse/data/aggregated/transaction/country/india/state/"
-agg_tran_list = os.listdir(path1)
+#Aggregated_insurance_df
+cursor.execute("SELECT * FROM aggregated_insurance")
+mydb.commit()
+table1 = cursor.fetchall()
 
-columns1 = {"States":[], "Years":[], "Quarter":[], "Transaction_type": [], "Transaction_count":[], "Transaction_amount":[]}
-
-for state in agg_tran_list:
-    current_states = path1 + state + "/"
-    agg_year_list = os.listdir(current_states)
-    
-    for year in agg_year_list:
-        current_year = current_states + year + "/"
-        agg_file_list = os.listdir(current_year)
-        
-        for file in agg_file_list:
-            current_file = current_year + file 
-            data = open(current_file,"r")
-
-            A = json.load(data)
-
-            for i in A["data"]["transactionData"]:
-                name = i["name"]
-                count = i["paymentInstruments"][0]["count"]
-                amount = i["paymentInstruments"][0]["count"]
-                columns1["Transaction_type"].append(name)
-                columns1["Transaction_count"].append(count)
-                columns1["Transaction_amount"].append(amount)
-                columns1["States"].append(state)
-                columns1["Years"].append(year)
-                columns1["Quarter"].append(int(file.strip(".json")))
+Aggregated_insurance = pd.DataFrame(table1,columns=("States","Years","Quarter","Transaction_type","Transaction_count","Transaction_amount"))
 
 
-Aggre_transaction = pd.DataFrame(columns1)
+#Aggregated_transacation_df
+cursor.execute("SELECT * FROM aggregated_transaction")
+mydb.commit()
+table2 = cursor.fetchall()
+
+Aggregated_transacation = pd.DataFrame(table2,columns=("States","Years","Quarter","Transaction_type","Transaction_count","Transaction_amount"))
 
 
+#Aggregated_user_df
+cursor.execute("SELECT * FROM aggregated_user")
+mydb.commit()
+table3 = cursor.fetchall()
 
-#Aggregated_user
+Aggregated_user= pd.DataFrame(table3,columns=("States","Years","Quarter","Brands","Transaction_count","Percentage"))
 
-path2 = "C:/Users/viren/OneDrive/Desktop/IIT-MADARAS(GUVI)/Phonepay project/pulse/data/aggregated/user/country/india/state/"
-agg_user_list = os.listdir(path2)
 
-columns2 = {"States":[], "Years":[], "Quarter":[], "Brands": [], "Transaction_count":[], "Percentage":[]}
+#Map_insurance_df
+cursor.execute("SELECT * FROM map_insurance")
+mydb.commit()
+table4 = cursor.fetchall()
 
-for state in agg_user_list:
-    current_states = path2 + state + "/"
-    agg_year_list = os.listdir(current_states)
+Map_insurance= pd.DataFrame(table4,columns=("States","Years","Quarter","Districts","Transaction_count","Transaction_amount"))
 
-    for year in agg_year_list:
-        current_year = current_states + year + "/"
-        agg_file_list = os.listdir(current_year)
 
-        for file in agg_file_list:
-            current_file = current_year + file
-            data = open(current_file,"r")
+#Map_transaction_df
+cursor.execute("SELECT * FROM map_transaction")
+mydb.commit()
+table5 = cursor.fetchall()
 
-            B = json.load(data)
-            
-            try:
-                for i in B["data"]["usersByDevice"]:
-                                brand = i["brand"]
-                                count = i["count"]
-                                percentage = i["percentage"]
-                                columns2["Brands"].append(brand)
-                                columns2["Transaction_count"].append(count)
-                                columns2["Percentage"].append(percentage)
-                                columns2["States"].append(state)
-                                columns2["Years"].append(year)
-                                columns2["Quarter"].append(int(file.strip(".json")))
+Map_transaction= pd.DataFrame(table5,columns=("States","Years","Quarter","Districts","Transaction_count","Transaction_amount"))
 
-            except:
+
+#Map_user_df
+cursor.execute("SELECT * FROM map_user")
+mydb.commit()
+table6 = cursor.fetchall()
+
+Map_user= pd.DataFrame(table6,columns=("States","Years","Quarter","Districts","RegisteredUsers","AppOpens"))
+
+
+#Top_Insurance_df
+cursor.execute("SELECT * FROM top_insurance")
+mydb.commit()
+table7 = cursor.fetchall()
+
+Top_Insurance= pd.DataFrame(table7,columns=("States","Years","Quarter","Pincodes","Transaction_count","Transaction_amount"))
+
+
+#Top_transaction _df
+cursor.execute("SELECT * FROM top_transaction")
+mydb.commit()
+table8 = cursor.fetchall()
+
+Top_transaction = pd.DataFrame(table8,columns=("States","Years","Quarter","Pincodes","Transaction_count","Transaction_amount"))
+
+
+#Top_user _df
+cursor.execute("SELECT * FROM top_user")
+mydb.commit()
+table9 = cursor.fetchall()
+
+Top_user = pd.DataFrame(table9,columns=("States","Years","Quarter","Pincodes","RegisteredUsers"))
+
+
+# creating function for Transaction_amount_count
+
+def Transaction_amount_count_Y(df,year):
+
+    tran_amount_count_year = df[df["Years"] == year]
+    tran_amount_count_year.reset_index(drop = True, inplace= True)
+
+    tran_amount_count_year_group = tran_amount_count_year.groupby("States")[["Transaction_count","Transaction_amount"]].sum()
+    tran_amount_count_year_group.reset_index(inplace=True)
+
+    fig_amount = px.bar(tran_amount_count_year_group, x="States", y="Transaction_amount", title=f"{year} TRANSACTION AMOUNT",
+                        color_discrete_sequence=px.colors.sequential.Aggrnyl)
+    st.plotly_chart(fig_amount)
+
+    fig_count = px.bar(tran_amount_count_year_group, x="States", y="Transaction_count", title=f"{year} TRANSACTION COUNT",
+                        color_discrete_sequence=px.colors.sequential.Blackbody_r)
+    st.plotly_chart( fig_count)
+
+
+#Streamlit part
+
+st.set_page_config(layout="wide")
+st.title("PHONEPE DATA VISUALIZATION AND EXPLORATION")
+
+with st.sidebar:
+
+    select = option_menu("Main Menu",["HOME", "DATA EXPLORATION", "TOP CHARTS"])
+
+    if select == "HOME":
+        pass
+
+    elif select == "DATA EXPLORATION":
+
+        tab1, tab2, tab3 = st.tabs(["Aggregated Analysis", "Map Analysis", "Top Analysis"])
+
+        with tab1:
+            method = st.radio("Select The Method",["Insurance Analysis", "Transaction Analysis", "User Analysis"])
+
+            if method == "Insurance Analysis":
+                years = st.slider("Select The Year",Aggregated_insurance["Years"].min(),Aggregated_insurance["Years"].max())
+                Transaction_amount_count_Y(Aggregated_insurance,years)
+
+            elif method == "Transaction Analysis":
                 pass
 
-Aggregated_user = pd.DataFrame(columns2)
+            elif method == "User Analysis":
+                pass
 
-           
+        with tab2:
+            method_2 = st.radio("Select The Method",["Map Insurance", "Map Transaction", "Map User"])
 
-#Aggregated_insurance
+            if method_2 == "Map Insurance":
+                pass
 
-path3 = "C:/Users/viren/OneDrive/Desktop/IIT-MADARAS(GUVI)/Phonepay project/pulse/data/aggregated/insurance/country/india/state/"
-agg_insurance_list = os.listdir(path3)
+            elif method_2 == "Map Transaction":
+                pass
 
-columns3 = {"States":[],"Years":[],"Quarter":[],"Transaction_type":[],"Transaction_count":[],"Transaction_amount":[]}
+            elif method_2 == "Map User":
+                pass
 
-for state in agg_insurance_list:
-    current_states = path3 + state + "/"
-    agg_year_list = os.listdir(current_states)
+        with tab3:
 
-    for year in agg_year_list:
-        current_year = current_states + year + "/"
-        agg_file_list = os.listdir(current_year)
+            method_3 = st.radio("Select The Method",["Top Insurance", "Top Transaction", "Top User"])
 
-        for file in agg_file_list:
-            current_file =  current_year + file
-            data = open(current_file,"r")
+            if method_3 == "Top Insurance":
+                pass
 
-            C = json.load(data)
+            elif method_3 == "Top Transaction":
+                pass
 
-           
-            for i in C["data"]["transactionData"]:
-                name = i["name"]
-                count = i["paymentInstruments"][0]["count"]
-                amount = i["paymentInstruments"][0]["amount"]
-                columns3["Transaction_type"].append(name)
-                columns3["Transaction_count"].append(count)
-                columns3["Transaction_amount"].append(amount)
-                columns3["States"].append(state)
-                columns3["Years"].append(year)
-                columns3["Quarter"].append(int(file.strip(".json")))
+            elif method_3 == "Top User":
+                pass
 
-
-Aggregated_insurance = pd.DataFrame(columns3)
-
-
-#Map_insurance 
-
-path4 = "C:/Users/viren/OneDrive/Desktop/IIT-MADARAS(GUVI)/Phonepay project/pulse/data/map/insurance/hover/country/india/state/"
-map_insurance_list = os.listdir(path4)
-
-columns4 = {"States":[],"Years":[],"Quarter":[],"Districts":[],"Transaction_count":[],"Transaction_amount":[]}
-
-for state in map_insurance_list:
-    current_states = path4 + state + "/"
-    agg_year_list = os.listdir(current_states)
-
-    for year in agg_year_list:
-        current_year = current_states + year + "/"
-        agg_file_list = os.listdir(current_year)
-
-        for file in agg_file_list:
-            current_file =  current_year + file
-            data = open(current_file,"r")
-
-            D = json.load(data)
-            
-            for i in D["data"]["hoverDataList"]:
-                name = i["name"]
-                count = i["metric"][0]["count"]
-                amount = i["metric"][0]["amount"]
-                columns4["Districts"].append(name)
-                columns4["Transaction_count"].append(count)
-                columns4["Transaction_amount"].append(amount)
-                columns4["States"].append(state)
-                columns4["Years"].append(year)
-                columns4["Quarter"].append(int(file.strip(".json")))
-
-
-Map_insurance = pd.DataFrame(columns4)
-
-
-
-#Map_transcation
-
-path5 = "C:/Users/viren/OneDrive/Desktop/IIT-MADARAS(GUVI)/Phonepay project/pulse/data/map/transaction/hover/country/india/state/"
-map_transaction_list = os.listdir(path5)
-
-columns5 = {"States":[],"Years":[],"Quarter":[],"Districts":[],"Transaction_count":[],"Transaction_amount":[]}
-
-for state in map_transaction_list:
-    current_states = path5 + state + "/"
-    map_year_list = os.listdir(current_states)
-
-    for year in map_year_list:
-        current_year = current_states + year + "/"
-        map_file_list = os.listdir(current_year)
-
-        for file in map_file_list:
-            current_file =   current_year + file
-            data = open(current_file,"r")
-
-            E = json.load(data)
-
-            for i in E["data"]["hoverDataList"]:
-                name = i["name"]
-                count = i["metric"][0]["count"]
-                amount = i["metric"][0]["amount"]
-                columns5["Districts"].append(name)
-                columns5["Transaction_count"].append(count)
-                columns5["Transaction_amount"].append(amount)
-                columns5["States"].append(state)
-                columns5["Years"].append(year)
-                columns5["Quarter"].append(int(file.strip(".json")))    
-
-Map_transcation = pd.DataFrame(columns5)
+    elif select == "TOP CHARTS":
+        pass
