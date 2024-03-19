15776036993c9eabe838a1a20a1326435e6130f2
virenemmanuel <viren.emmanuel@gmail.com>
2024-03-20 04:57:11 +0530

    commit some streamlit code

--- Phonepe.py
+++ Phonepe.py
@@ -127,7 +127,7 @@ def Transaction_amount_count_Y(df,year):
 
         states_name.sort()
 
-    with col2:
+    
         fig_india_1 = px.choropleth(   tran_amount_count_year_group, geojson= data_1, locations= "States", featureidkey= "properties.ST_NM",
                                     color= "Transaction_amount", color_continuous_scale= "Rainbow", 
                                     range_color=(tran_amount_count_year_group["Transaction_amount"].min(),tran_amount_count_year_group["Transaction_amount"].max()),
@@ -136,16 +136,90 @@ def Transaction_amount_count_Y(df,year):
         fig_india_1.update_geos(visible = False)
         st.plotly_chart(fig_india_1)
 
+    with col2:
+        fig_india_2 = px.choropleth(   tran_amount_count_year_group, geojson= data_1, locations= "States", featureidkey= "properties.ST_NM",
+                                color= "Transaction_count", color_continuous_scale= "Rainbow", 
+                                range_color=(tran_amount_count_year_group["Transaction_count"].min(),tran_amount_count_year_group["Transaction_count"].max()),
+                                hover_name= "States", title = f"{year} TRANSACTION COUNT",fitbounds= "locations",
+                                height= 600,width= 600)
+        fig_india_2.update_geos(visible = False)
+        st.plotly_chart(fig_india_2)
 
-    fig_india_2 = px.choropleth(   tran_amount_count_year_group, geojson= data_1, locations= "States", featureidkey= "properties.ST_NM",
-                            color= "Transaction_count", color_continuous_scale= "Rainbow", 
-                            range_color=(tran_amount_count_year_group["Transaction_count"].min(),tran_amount_count_year_group["Transaction_count"].max()),
-                            hover_name= "States", title = f"{year} TRANSACTION COUNT",fitbounds= "locations",
-                            height= 600,width= 600)
-    fig_india_2.update_geos(visible = False)
-    st.plotly_chart(fig_india_2)
+    return tran_amount_count_year
 
 
+# creating function for Transaction_amount_count
+
+def Transaction_amount_count_Y_Q(df, quarter):
+    tran_amount_count_year = df[df["Quarter"] ==  quarter]
+    tran_amount_count_year.reset_index(drop = True, inplace= True)
+
+    tran_amount_count_year_group = tran_amount_count_year.groupby("States")[["Transaction_count","Transaction_amount"]].sum()
+    tran_amount_count_year_group.reset_index(inplace=True)
+
+    col1,col2 = st.columns(2)
+    with col1:
+        fig_amount = px.bar(tran_amount_count_year_group, x="States", y="Transaction_amount", title=f"{tran_amount_count_year['Years'].min()} YEAR {quarter} Quarter TRANSACTION AMOUNT",
+                            color_discrete_sequence=px.colors.sequential.Aggrnyl)
+        st.plotly_chart(fig_amount)
+
+    with col2:
+        fig_count = px.bar(tran_amount_count_year_group, x="States", y="Transaction_count", title=f"{tran_amount_count_year['Years'].min()} YEAR {quarter} Quarter TRANSACTION COUNT",
+                            color_discrete_sequence=px.colors.sequential.OrRd_r)
+        st.plotly_chart(fig_count)
+
+
+    col1,col2 = st.columns(2)
+    with col1:
+        url = "https://gist.githubusercontent.com/jbrobst/56c13bbbf9d97d187fea01ca62ea5112/raw/e388c4cae20aa53cb5090210a42ebb9b765c0a36/india_states.geojson"
+        response = requests.get(url)
+        data_1 = json.loads(response.content)
+        states_name = []
+        for feature in data_1["features"]:
+            states_name.append(feature["properties"]["ST_NM"])
+
+        states_name.sort()
+
+        fig_india_1 = px.choropleth(   tran_amount_count_year_group, geojson= data_1, locations= "States", featureidkey= "properties.ST_NM",
+                                    color= "Transaction_amount", color_continuous_scale= "Rainbow", 
+                                    range_color=(tran_amount_count_year_group["Transaction_amount"].min(),tran_amount_count_year_group["Transaction_amount"].max()),
+                                    hover_name= "States", title = f"{tran_amount_count_year['Years'].min()} YEAR {quarter} Quarter TRANSACTION AMOUNT",fitbounds= "locations",
+                                    height= 600,width= 600)
+        fig_india_1.update_geos(visible = False)
+        st.plotly_chart(fig_india_1)
+
+    with col2:
+        fig_india_2 = px.choropleth(   tran_amount_count_year_group, geojson= data_1, locations= "States", featureidkey= "properties.ST_NM",
+                                color= "Transaction_count", color_continuous_scale= "Rainbow", 
+                                range_color=(tran_amount_count_year_group["Transaction_count"].min(),tran_amount_count_year_group["Transaction_count"].max()),
+                                hover_name= "States", title = f"{tran_amount_count_year['Years'].min()} YEAR {quarter} Quarter TRANSACTION COUNT",fitbounds= "locations",
+                                height= 600,width= 600)
+        fig_india_2.update_geos(visible = False)
+        st.plotly_chart(fig_india_2)
+
+    return tran_amount_count_year
+    
+
+
+def Aggre_tran_Transaction_type(df,state):
+
+    tran_amount_count_year = df[df["States"] == state]
+    tran_amount_count_year.reset_index(drop = True, inplace= True)
+
+    tran_amount_count_year_group = tran_amount_count_year.groupby("Transaction_type")[["Transaction_count","Transaction_amount"]].sum()
+    tran_amount_count_year_group.reset_index(inplace=True)
+
+    col1,col2 = st.columns(2)
+    with col1:
+        fig_pie_1 = px.pie(data_frame=tran_amount_count_year_group, names="Transaction_type",values="Transaction_amount",
+                        width = 600,title=f"{state.upper()} TRANSACTION AMOUNT", hole= 0.5)
+
+        st.plotly_chart(fig_pie_1)
+    with col2:
+        fig_pie_2 = px.pie(data_frame=tran_amount_count_year_group, names="Transaction_type",values="Transaction_count",
+                        width = 600,title=f"{state.upper()} TRANSACTION COUNT", hole= 0.5)
+
+        st.plotly_chart(fig_pie_2)
 
 #Streamlit part
 
@@ -171,11 +245,48 @@ elif select == "DATA EXPLORATION":
             col1,col2 = st.columns(2)
             with col1:
 
-                years = st.slider("Select The Year",Aggregated_insurance["Years"].min(),Aggregated_insurance["Years"].max())
-                Transaction_amount_count_Y(Aggregated_insurance,years)
+                years = st.slider("Select The Year",Aggregated_insurance["Years"].min(),Aggregated_insurance["Years"].max(),Aggregated_insurance["Years"].min())
+            tran_amount_count_year = Transaction_amount_count_Y(Aggregated_insurance,years)
+
+            col1,col2 = st.columns(2)
+            with col1:
+
+                quarter = st.slider("Select The Quarter",tran_amount_count_year["Quarter"].min(),tran_amount_count_year["Quarter"].max(),
+                                         tran_amount_count_year["Quarter"].min())
+            
+            Transaction_amount_count_Y_Q(tran_amount_count_year, quarter)
+
+
 
         elif method == "Transaction Analysis":
-            pass
+
+             col1,col2 = st.columns(2)
+             with col1:
+
+                years = st.slider("Select The Year",Aggregated_transacation["Years"].min(),Aggregated_transacation["Years"].max(),Aggregated_transacation["Years"].min())
+             Aggre_tran_amount_count_year = Transaction_amount_count_Y(Aggregated_transacation,years)
+
+             col1,col2 = st.columns(2)
+             with col1:
+                 states = st.selectbox("Select The State", Aggre_tran_amount_count_year["States"].unique())
+
+             Aggre_tran_Transaction_type(Aggre_tran_amount_count_year,states)
+
+
+             col1,col2 = st.columns(2)
+             with col1:
+
+                quarter = st.slider("Select The Quarter", Aggre_tran_amount_count_year["Quarter"].min(), Aggre_tran_amount_count_year["Quarter"].max(),
+                                          Aggre_tran_amount_count_year["Quarter"].min())
+            
+             Aggre_tran_amount_count_year_Q = Transaction_amount_count_Y_Q(Aggre_tran_amount_count_year, quarter) 
+
+             col1,col2 = st.columns(2)
+             with col1:
+                 states = st.selectbox("Select The State_ty", Aggre_tran_amount_count_year_Q["States"].unique())
+
+             Aggre_tran_Transaction_type(Aggre_tran_amount_count_year_Q,states)
+            
 
         elif method == "User Analysis":
             pass
